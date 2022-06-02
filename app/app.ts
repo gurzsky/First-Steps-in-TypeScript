e6655ffb0fb6ed9b7e848ce489f54808150bab46
gurzsky <gurski.ce@gmail.com>
2022-06-02 17:03:48 -0300

    included StrictNullChecks

--- app/app.ts
+++ app/app.ts
@@ -2,7 +2,12 @@ import { NegociacaoController } from "./controllers/negociacao-controller.js";
 
 const controller = new NegociacaoController();
 const form = document.querySelector('.form');
-form.addEventListener('submit', event => {
-    event.preventDefault();
-    controller.adiciona();
-});
+if (form) {
+    form.addEventListener('submit', event => {
+        event.preventDefault();
+        controller.adiciona();
+    });
+} else {
+    throw Error('Nao foi possivel inicializar a aplicacao. Verifique se o form esta preenchido!');
+}
+
